5da9069cf72e38b1e69e329f9491c58f7b4e4d85
agdgom18 <agdgomelashvili18@gmail.com>
2024-01-19 12:25:38 +0400

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
-
+import Account from './routes/Account'
+import Home from './routes/Home'
+import SignIn from './routes/SignIn'
+import SignUp from './routes/SignUp'
 function App() {
 	return (
 		<>
 			<ThemeProvider>
 				<NavBar />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/signin' element={<SignIn />} />
+					<Route path='/signUp' element={<SignUp />} />
+					<Route path='/account' element={<Account />} />
+				</Routes>
 			</ThemeProvider>
 		</>
 	)
